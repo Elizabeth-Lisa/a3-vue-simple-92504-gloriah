ec3b35cccf30d83e427e303e3860498f1525f955
92504-gloriah <kerubo.gloriah@strathmore.edu>
2020-03-27 01:26:50 +0300

    IDW JS App

--- js/app.js
+++ js/app.js
@@ -8,5 +8,51 @@
   2. BMI Formula : kg/(height in metres * height in metres)
   3. jQuery for modifying bound name result(color change)
   4. Enable tooltips
+  */
 
-*/
+function calculate_weight(){
+
+  var user_name = document.getElementById('name').value;
+  var user_age = document.getElementById('age').value;
+  var user_gender = document.getElementById('gender').value;
+  var user_height = document.getElementById('height').value;
+
+  if (user_gender == 'male'){
+    ideal_weight = 50;
+    additional_weight = 0;
+    
+    for (var i = user_height; i > 5; i++) {
+      additional_weight += 2.3
+    }
+
+    total = ideal_weight + additional_weight;
+
+    var results_span = document.getElementById('results')
+    results_span.innerHTML = total;
+  }
+
+  else if (user_gender == 'female'){
+
+    console.log("female");
+  }
+
+
+}
+
+
+/*function calculate_weight(){
+
+  var user_name = document.getElementById('name').value;
+  var user_age = document.getElementById('age').value;
+  var user_gender = document.getElementById('gender').value;
+  var user_height = document.getElementById('height').value;
+
+  bmi = user_height * user_age;
+
+  var results_span = document.getElementById('results')
+  results_span.innerHTML = bmi;
+
+
+  }
+
+}*/

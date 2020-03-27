9456a02bcd5cb19271f9dd08e9080d222924efac
92504-gloriah <kerubo.gloriah@strathmore.edu>
2020-03-27 16:52:03 +0300

    Fixed Height Conversion

--- js/app.js
+++ js/app.js
@@ -28,31 +28,31 @@
           //set ideal weight to a constant of 50
           var ideal_weight = 50;
 
-          //set additional weight to zero, to be incremented based on weight
-          var additional_weight = 0;
-
           //calculate additional Weight
+          this.height = this.height/30.48;  //convert to feet
+
           if (this.height > 5){
             var extre_height = this.height - 5;
+            extre_height = extre_height * 12; // convert to inch
             additional_weight = 2.3 * extre_height;
             console.log(extre_height);
             }
           }
 
+          //male weight calculation here
+          if (this.gender == 'female'){
+            //set ideal weight to a constant of 50
+            var ideal_weight = 49;
 
-        //female weight calculation here
-        else if (this.gender == 'female'){
-          var ideal_weight = 49;
-
-          //set additional weight to zero, to be incremented based on weight
-          var additional_weight = 0;
-
-          //calculate additional Weight
-          if (this.height > 5){
-            var extre_height = this.height - 5;
-            additional_weight = 1.7 * extre_height;
+            //calculate additional Weight
+            this.height = this.height/30.48;  //convert to feet
+            console.log(this.height);
+            if (this.height > 5){
+              var extre_height = this.height - 5;
+              extre_height = extre_height * 12; // convert to inch
+              additional_weight = 1.7 * extre_height;
+              }
             }
-        }
 
         //calculate total weight
         total = ideal_weight + additional_weight;
@@ -62,8 +62,8 @@
         results_span.innerHTML = "Ideal Weight: " + Math.trunc(total) + "kg";
 
         //create limits for weight Range
-        var weight_range_lower = Math.trunc(total - 12);
-        var weight_range_upper = Math.trunc(total + 12);
+        var weight_range_lower = Math.trunc(total - 15);
+        var weight_range_upper = Math.trunc(total + 6);
 
         //Didplay weight Range
         var results_span = document.getElementById('range');
